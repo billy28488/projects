c4e2fd5195248e39919a28cc17ddaee620b01cf8
billy28488 <billydramis@yahoo.com>
2020-07-28 18:15:10 +0100

    New project

--- gitProject/gitProject/Program.cs
+++ gitProject/gitProject/Program.cs
@@ -10,6 +10,8 @@ namespace gitProject
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello");
+            string x = Console.ReadLine();
         }
     }
 }
